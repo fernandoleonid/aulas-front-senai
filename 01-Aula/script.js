32116cbb91db843939982dc0354f99de7f4d4caa
unknown <fernandoleonid@gmail.com>
2020-02-13 08:49:49 -0300

    Adicionando função de exibir e ocultar elementos

--- 01-Aula/script.js
+++ 01-Aula/script.js
@@ -1,18 +1,20 @@
+// Fernando Leonid
 const $calcular = document.getElementById('calcular');
+const $exibir = document.getElementById('exibir');
+const $ocultar = document.getElementById('ocultar');
+const $conteiner = document.querySelector ('.conteiner');
 
-const calcularMedia = ( n1, n2 ) => (parseInt(n1) + parseInt(n2) ) / 2;
-
+const calcularMedia = ( n1, n2 ) => ( parseInt( +n1 ) + parseInt( +n2 ) ) / 2;
+ 
 const verificarSituacao = ( media ) => media >= 5 ? "Aprovado" : "Reprovado";
 
-
 const exibirMedia = () => {
-    const $nome = document.getElementById('nome');
     const nota1 = document.getElementById('nota1').value;
     const nota2 = document.getElementById('nota2').value;
     const $media = document.getElementById('media');
     const $situacao = document.getElementById('situacao');
 
-    const media = calcularMedia ( nota1, nota2 );
+    const media = calcularMedia ( nota1, nota2 ) ;
 
     $situacao.value = verificarSituacao ( media );
 
@@ -20,7 +22,7 @@ const exibirMedia = () => {
 
 }
 
-const calcularConceito =  () => {
+const defineConceito =  () => {
     const media = document.getElementById ('media').value
     const $conceito = document.getElementById ('conceito');
 
@@ -30,18 +32,22 @@ const calcularConceito =  () => {
         $conceito.value = "D";
     }else if ( media < 8 ) {
         $conceito.value = "C";
-    }else if ( media < 10) {
+    }else if ( media < 10 ) {
         $conceito.value = "B";
     }else {
         $conceito.value = "A";
     }
 }
 
-
-const calcular = () => {
+const exibirResultados = () => {
     exibirMedia();
-    calcularConceito();
+    defineConceito();
 }
 
+const exibir = ( el ) =>   el.classList.add ( 'exibir' );
+const ocultar = ( el ) =>   el.classList.remove ( 'exibir' );
 
-$calcular.addEventListener('click', calcular);
+
+$calcular.addEventListener('click', exibirResultados);
+$exibir.addEventListener('click',( ) => exibir ( $conteiner ));
+$ocultar.addEventListener('click',( ) => ocultar ( $conteiner ));
