bf6865b3bedd9f65eebcb0aefeb7b2c2f97406f7
unknown <fernandoleonid@gmail.com>
2020-03-13 01:05:00 -0300

    aula finalizada

---
+++ 06.0-slide/slide.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const loadImages = () => {
+    const arrImg = [];
+    for ( let i=1; i<=5; i++ ) {
+        arrImg.push (`./img/imagem (${i}).jpg`);
+    }
+    return arrImg;
+}
+
+const insertImages = ( images ) => {
+    const htmlImg = images.map ( img => `<img class='images' src='${img}'>`).join(" ");
+    
+    const $container = document.getElementById('container');
+    const $containerImages = document.createElement ('div');
+    const $next = document.getElementById ('next');
+    $containerImages.id = 'containerImages';
+    $containerImages.innerHTML = htmlImg;
+    $container.insertBefore ( $containerImages, $next );
+
+}
+
+const next = () => {
+    const $img = document.querySelectorAll ('.images');
+    const max = ($img.length - 1) * (-50);
+    
+    let marginLeft = +($img[0].style.marginLeft.replace("vw",""));
+
+    if ( marginLeft == max ) {
+        marginLeft = 0;
+    }
+
+    $img[0].style.marginLeft = (marginLeft - 50) + "vw";
+    
+}
+
+
+
+insertImages(loadImages());
+
+document.getElementById('next').addEventListener ('click',next);
+document.getElementById('prev').addEventListener ('click',prev);
